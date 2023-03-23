e21a027e809494c0f834309bf8b970aa052c4255
kwon woo <dirn0568@naver.com>
2023-03-23 20:53:13 +0900

    서블릿 컨테이너 추가

--- src/main/java/com/example/webrtckw/config/WebSocketConfig.java
+++ src/main/java/com/example/webrtckw/config/WebSocketConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
